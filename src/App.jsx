f22b020fe27ee70c9130b9783ad3007a7fecf703
gaurav saini <sainigauravkumar@gmail.com>
2025-12-26 14:27:19 +0530

    setup vite-react & routing

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import Login from "./components/Login";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Body from "./components/Body";
+import Profile from "./components/Profile";
+const App = () => {
+  return (
+    <>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      
+    </>
+  );
+};
+
+export default App;
